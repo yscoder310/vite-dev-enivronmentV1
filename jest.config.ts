ec9671fd25eec843650f196758e0f5f5250e8e9a
CodeSandbox <support@codesandbox.io>
2023-10-27 07:10:35 +0000

    Sandbox snapshot commit

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>setupTests.ts"],
+  testMatch: ["<rootDir>/src/**/*.(test|spec).(ts|tsx)"],
+  testPathIgnorePatterns: ["<rootDir>/tests/e2e/"],
+  transform: {
+    "^.+\\.svg$": "jest-transform-stub",
+    "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "jest-transform-stub",
+  },
+};
