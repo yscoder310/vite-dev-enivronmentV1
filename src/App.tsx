5078216ae97871fb5b9c27411fe672a5a821e379
Yash Singhal <75132721+ysinghal310@users.noreply.github.com>
2023-10-27 07:45:55 +0000

    checking husky v1

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
 
   return (
     <div className="App">
+      Hello world
       <div>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
